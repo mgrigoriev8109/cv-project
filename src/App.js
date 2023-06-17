a0ccdc0e3c4d49db80c186a2c1886692a1288e42
mgrigoriev8109 <mikhailgrigoriev92@gmail.com>
2023-06-16 23:12:39 -0400

    chore: modify boilerplate

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
-function App() {
-  return (
-    <div className="App">
+import React, { Component } from 'react';
+import SubmittedCV from './components/SubmittedCV.js';
 
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {}
+
+    this.handleEdit = this.handleEdit.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    // do stuff with submit button
+  }
+
+  handleEdit(event) {
+    // do stuff with edit button
+  }
+
+  render() {
+    return (
+      <div>
+
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name <input name="name" type="text" />
+          </label>
+
+          <input type="submit" value="Submit" />
+        </form>
+
+        <SubmittedCV data={this.state.data} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/SubmittedCV.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Overview extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div className="Overview">
+                {data}
+            </div>
+        )
+    }
+}
+
+export default Overview
